fa094f0d0fd69a379be7c9018613f1521751c617
NicholasJohnson2020 <35481278+NicholasJohnson2020@users.noreply.github.com>
2018-08-17 15:19:54 +0100

    Formatted Tutorial 4 Code

--- example_4/Tutorial_4_Code.R
+++ example_4/Tutorial_4_Code.R
@@ -1,90 +1,84 @@
-# An object holding all the coefficients used in the model. The setting of the
-# names and the initialisation is done simultaneusly by editing this.
-# coef is REQUIRED by mcara()!
-coef <- c(i=1.0, j=1.0, k=1.0,
-          l=1.0, m=1.0, n=1.0)
-          
-
-# An object holding the target observables for the optimisation.
-# target is REQUIRED by mcara()!
-target <- c(Ca=29.7548, Cg=20.23669, Ct=29.793, Cc=20.21551)
-
-
-
 # An object that is created by setting the state names and the initial values.
 # The values are to be given in any unit. The sum will stay constant across the
 # dynamics of the system. For instance, in case those are the fractions in %,
 # the sum should be 100% and will stay the same in the solution as well.
-state  <- c(Ca=100/4, Cg=100/4, Ct=100/4, Cc=100/4)
+state  <- c(cA=100/3, cB=100/3, cC=100/3, cAP=0, cBP=0, cCP=0)
+
+# An object holding the target observables for the optimisation.
+target <- c(cA=40, cB=20, cC=40, cAP=0, cBP=0, cCP=0)
 
 # An object holding the model definition. It should contain equations that use
 # the objects with the names specified within state and coef above, and should
 # have equations that assign the outcomes to new objects that have the same
 # order and names as specified in state, but with "d" at the beginning.
 #---------------------------------
-model <- function(t, state, coef){
-
-  with( as.list(c(state, coef)), {
+model <- function(t, state, K){
+  
+  with( as.list(c(state, K)), {
     # rate of change
-       dCa <- i*Cc+l*Ct+m*Cg-(j+l+n)*Ca
-       dCg <- n*Ca+k*Cc+j*Ct-(m+i+k)*Cg
-       dCt <- l*Ca+i*Cg+m*Cc-(l+n+j)*Ct
-       dCc <- j*Ca+n*Ct+k*Cg-(i+m+k)*Cc
+    dcA  <- -k1*cA+k2*cAP*cB
+    dcB  <- -k2*cAP*cB+k3*cBP*cC
+    dcC  <- -k3*cBP*cC+k4*cCP
+    dcAP <- -dcA
+    dcBP <- -dcB
+    dcCP <- -dcC
     # return the rate of change
-    list(c(dCa, dCg, dCt, dCc))
-  }) # end with(as.list ...
-
+    list(c(dcA, dcB, dcC, dcAP, dcBP, dcCP))
+  })
 }
-#---------------------------------
 
-# Helper function.
-#---------------------------------
-GetModelOut <- function(state=state, model=model, coef=coef){
+DATA <- NULL
+DATA$state <- state
+DATA$target <- target
+DATA$model <- model
+
+# An object holding all the coefficients used in the model. The setting of the
+# names and the initialisation is done simultaneusly by editing this.
+
+################################################################################
+K <- c(k1=1.0, k2=1.0, k3=1.0, k4=1.0)
+################################################################################
+
+################################################################################
+m <- function(K, DATA){
+  library(deSolve)
+  
+  state <- DATA$state
+  model <- DATA$model
+  
   span = 10.0  # byr
-  library("deSolve")
 
   times <- c(0, span)
-  out   <- ode(y=state, times=times, func=model, parms=coef)[2,2:(length(state)+1)]
+  out   <- ode(y=state, times=times, func=model, parms=K)[2,2:(length(state)+1)]
   return(out)
 }
-#---------------------------------
+################################################################################
 
-# Helper function to assess the agreement quality.
-#---------------------------------
-GetPseudoE <- function(sol=sol, target=target, type="RMSE"){
+################################################################################
+u <- function(O, DATA){
+  target <- DATA$target
   RESULT <- NULL
-  if(type=="RMSE"){
-    RESULT$Q <- sqrt(mean((sol-target)^2)) # measure of the fit quality
-    RESULT$E <- RESULT$Q # the pseudoenergy derived from the above measure
-  }
+  RESULT$Q <- sqrt(mean((O-target)^2)) # measure of the fit quality
+  RESULT$E <- RESULT$Q # the pseudoenergy derived from the above measure
+
   return(RESULT)
 }
-#---------------------------------
+################################################################################
 
-
-# The modelling core function that is called in the main mcara() optimiser.
-# Takes - coef and other arguments specified in this file (model.R), outputs -
-# an object with $sol, $sol.perf and $coef.new components required by mcara.R.
-#---------------------------------
-MDLCore <- function(coef=coef, move.step=0.005, state=state,
-                    model=model, target=target){                               
-  coef.new <- coef
-  coef.ind.toalter <- sample(size=1, x=1:length(coef.new))
+################################################################################
+r <- function(K){                               
+  K.new <- K
+  # Randomly selecting a coefficient to alter:
+  K.ind.toalter <- sample(size=1, x=1:length(K.new))
   # Creating a potentially new set of coefficients where one entry is altered
-  # by either +move.step or -move.step:
-  coef.new[coef.ind.toalter] <- coef.new[coef.ind.toalter] +
-                                sample(size=1, x=c(-move.step, move.step))
-  sol      <- GetModelOut(state=state, model=model, coef=coef.new)
-  sol.perf <- GetPseudoE(sol=sol, target=target, type="RMSE")
+  # by either +move.step or -move.step, also randomly selected:
+  move.step <- 0.0005
+  K.new[K.ind.toalter] <- K.new[K.ind.toalter] + sample(size=1, x=c(-move.step, move.step))
   
-  RESULT          <- NULL
-  RESULT$sol      <- sol
-  RESULT$coef.new <- coef.new
-  RESULT$sol.perf <- sol.perf
-  return(RESULT)
-
+  ## Setting the negative coefficients to 0
+  neg.ind <- which(K.new < 0)
+  if(length(neg.ind)>0){ K.new[neg.ind] <- 0 }
+  
+  return(K.new)
 }
-#---------------------------------
-
-
 ################################################################################
