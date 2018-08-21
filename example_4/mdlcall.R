fed1f620a94f970ea4682e20fa327a30df19c967
NicholasJohnson2020 <35481278+NicholasJohnson2020@users.noreply.github.com>
2018-08-21 10:29:17 +0100

    Updated Tutorial 4

--- example_4/mdlcall.R
+++
@@ -1,7 +0,0 @@
-#-- Executing the modelling function. Takes - coef and other arguments
-#-- specified in model.R, outputs - $sol, $sol.perf and $coef.new as an mdl object.
-mdl <- MDLCore(coef=coef, 
-               move.step=0.002,
-               state=state,
-               model=model,
-               target=target) 
